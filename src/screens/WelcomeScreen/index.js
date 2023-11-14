57d363f6dda0a981872be42be8cc2ff6d60aceec
Md Minhazur Rahman <rafi.rahman03@gmail.com>
2023-11-14 14:03:21 +0600

    welcome screen animation bug fixed.

--- src/screens/WelcomeScreen/index.js
+++ src/screens/WelcomeScreen/index.js
@@ -2,7 +2,7 @@ import { useNavigation } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 import React, { useEffect } from "react";
 import { Image, Text, View } from "react-native";
-import { useSharedValue, withSpring } from "react-native-reanimated";
+import Animated, { useSharedValue, withSpring } from "react-native-reanimated";
 import { heightPercentageToDP as hp } from "react-native-responsive-screen";
 import logo from "../../../assets/images/welcome.png";
 
@@ -15,28 +15,31 @@ export default function WelcomeScreen() {
     outerRing.value = 0;
     innerRing.value = 0;
     setTimeout(
-      () => (outerRing.value = withSpring(outerRing.value+ hp(5))),
+      () => (outerRing.value = withSpring(outerRing.value + hp(5))),
       100
     );
     setTimeout(
-      () => (innerRing.value = withSpring(innerRing.value+ hp(5.5))),
+      () => (innerRing.value = withSpring(innerRing.value + hp(5.5))),
       300
     );
 
-    setTimeout(
-        () => navigation.navigate("Home"),
-        2500
-      );
+    setTimeout(() => navigation.navigate("Home"), 2500);
   }, []);
 
   return (
     <View className="flex-1 justify-center items-center space-y-10 bg-amber-500">
       <StatusBar style="light" />
-      <View className="bg-white/20 rounded-full" style={{ padding: hp(5) }}>
-        <View className="bg-white/20 rounded-full" style={{ padding: hp(4.5) }}>
+      <Animated.View
+        className="bg-white/20 rounded-full"
+        style={{ padding: outerRing }}
+      >
+        <Animated.View
+          className="bg-white/20 rounded-full"
+          style={{ padding: innerRing }}
+        >
           <Image source={logo} style={{ width: 200, height: 200 }} />
-        </View>
-      </View>
+        </Animated.View>
+      </Animated.View>
       <View className="flex items-center space-y-2">
         <Text
           className=" font-bold text-white tracking-widest"
