16058bdd897a7ba88baa270da7cfa3876ff6e200
Md Minhazur Rahman <rafi.rahman03@gmail.com>
2023-11-14 14:03:52 +0600

    categories component created.

---
+++ src/components/Categories/index.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
+import Animated, { FadeInDown } from "react-native-reanimated";
+import { heightPercentageToDP as hp } from "react-native-responsive-screen";
+
+export default function Categories({
+  categories,
+  activeCategory,
+  handleChangeCategory,
+}) {
+  return (
+    <Animated.View entering={FadeInDown.duration(500).springify()}>
+      <ScrollView
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ paddingHorizontal: 15 }}
+        className="space-x-4"
+      >
+        {categories.map((category, index) => {
+          return (
+            <TouchableOpacity
+              key={index}
+              onPress={() => handleChangeCategory(category.strCategory)}
+              className="flex items-center space-y-1"
+            >
+              <View
+                className={
+                  category.strCategory === activeCategory
+                    ? "rounded-full p-[6px] bg-amber-400"
+                    : "rounded-full p-[6px] bg-black/10"
+                }
+              >
+                <Image
+                  source={{ uri: category.strCategoryThumb }}
+                  style={{ width: hp(6), height: hp(6) }}
+                  className={"rounded-full"}
+                />
+              </View>
+              <Text className="text-neutral-600" style={{ fontSize: hp(1.6) }}>
+                {category.strCategory}
+              </Text>
+            </TouchableOpacity>
+          );
+        })}
+      </ScrollView>
+    </Animated.View>
+  );
+}
